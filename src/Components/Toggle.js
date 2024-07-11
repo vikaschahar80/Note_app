367a9c36893cab73fbda976f8425b691a484aecd
vikaschahar80 <vikaschahar9664@gmail.com>
2024-07-11 21:05:09 +0530

    first commit

---
+++ src/Components/Toggle.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Toggle = () => {
+  return (
+    <div className='toggle'> 
+        <input type="checkbox" class="checkbox"/>      
+    </div>
+  )
+}
+
+export default Toggle
